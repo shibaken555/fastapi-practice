3fbd5d1afb97c99a1b5382ebe46c16d4aedb277d
shibaken <ychaishan@gmail.com>
2026-02-08 00:52:14 +0900

    複数の企業の株価を期間を問わずに取得できるように修正

--- backend/app/companies/stock_price/router.py
+++ backend/app/companies/stock_price/router.py
@@ -7,14 +7,13 @@ router = APIRouter()
 
 
 # 入力したティッカーシンボルに合致する企業の株価を返却するAPI
-# 複数の企業の株価が取得出来る
-@router.get("/compare_stocks/")
-async def compare_stocks(ticker_symbol: Annotated[list[str], Query()] = None):
+@router.get("/fetch_price")
+async def fetch_price(ticker_symbol: Annotated[list[str], Query()], period: str):
     tickers = yf.Tickers(ticker_symbol)
     # 全企業の株価データを格納するリスト
     all_price_data = []
     for symbol in ticker_symbol:
-        prices = tickers.tickers[symbol].history(period="5d")
+        prices = tickers.tickers[symbol].history(period=period)
         price_data = prices.reset_index().to_dict(orient="records")
         # 各ティッカーのデータを辞書形式でリストに追加
         all_price_data.append({"ticker_symbol": symbol, "price_data": price_data})
