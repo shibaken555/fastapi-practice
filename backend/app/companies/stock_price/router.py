e97c1914a654a6f6d2cb6f0c7844ad6640a15a38
shibaken <ychaishan@gmail.com>
2026-02-08 01:28:33 +0900

    一部修正

--- backend/app/companies/stock_price/router.py
+++ backend/app/companies/stock_price/router.py
@@ -7,14 +7,16 @@ router = APIRouter()
 
 
 # 入力したティッカーシンボルに合致する企業の株価を返却するAPI
-@router.get("/fetch_price")
-async def fetch_price(ticker_symbol: Annotated[list[str], Query()], period: str):
-    tickers = yf.Tickers(ticker_symbol)
-    # 全企業の株価データを格納するリスト
+@router.get("/companies/stock_price")
+async def fetch_price(
+    ticker_symbol: Annotated[list[str], Query()], period: str = Query(default="1d")
+):
+    tickers = yf.Tickers(" ".join(ticker_symbol))
     all_price_data = []
     for symbol in ticker_symbol:
         prices = tickers.tickers[symbol].history(period=period)
-        price_data = prices.reset_index().to_dict(orient="records")
-        # 各ティッカーのデータを辞書形式でリストに追加
+        prices_dict = prices.reset_index()
+        prices_dict["Date"] = prices_dict["Date"].dt.strftime("%Y-%m-%d")
+        price_data = prices_dict.to_dict(orient="records")
         all_price_data.append({"ticker_symbol": symbol, "price_data": price_data})
     return all_price_data
