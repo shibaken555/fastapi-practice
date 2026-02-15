4e8f152e5de53f6135257c08500ea48c8a15d041
shibaken <ychaishan@gmail.com>
2026-02-15 21:42:50 +0900

    動くように修正

--- backend/app/companies/stock_price/router.py
+++ backend/app/companies/stock_price/router.py
@@ -8,15 +8,13 @@ router = APIRouter()
 
 # 入力したティッカーシンボルに合致する企業の株価を返却するAPI
 @router.get("/companies/stock_price")
-async def fetch_price(
-    ticker_symbol: Annotated[list[str], Query()], period: str = Query(default="1d")
-):
-    tickers = yf.Tickers(" ".join(ticker_symbol))
-    all_price_data = []
+async def fetch_stock_price(ticker_symbol: Annotated[list[str], Query()],period:str):
+    req_tickers = yf.Tickers(ticker_symbol)
+    all_prices_data = []
     for symbol in ticker_symbol:
-        prices = tickers.tickers[symbol].history(period=period)
-        prices_dict = prices.reset_index()
-        prices_dict["Date"] = prices_dict["Date"].dt.strftime("%Y-%m-%d")
-        price_data = prices_dict.to_dict(orient="records")
-        all_price_data.append({"ticker_symbol": symbol, "price_data": price_data})
-    return all_price_data
+        stock_prices = req_tickers.tickers[symbol].history(period=period)
+        stock_prices_dict = stock_prices.reset_index().to_dict(orient="records")
+        all_prices_data.append(
+            {"ticker_symbol": symbol, "price_data": stock_prices_dict}
+        )
+    return all_prices_data
