5298a11496c7e7e6304f2cba8c0c31dfd35d2238
shibaken <ychaishan@gmail.com>
2026-07-09 06:31:27 +0900

    DDDを意識したディレクトリ構成に変更するための対応

---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from src.presentation.information import router as information_router
+from src.presentation.stock_price import router as stock_price_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(information_router.router)
+app.include_router(stock_price_router.router)
